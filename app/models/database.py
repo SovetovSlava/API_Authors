ae8eebd8e734e3c9d6e1d3b5010d4bb8067f29d6
SovetovSlava <slava.sovetov.60@gmail.com>
2021-08-02 13:52:05 +0300

    changed bad author_id raise exeption to

--- app/models/database.py
+++ app/models/database.py
@@ -6,7 +6,6 @@ DB_USER = environ.get("DB_USER", "user")
 DB_PASSWORD = environ.get("DB_PASSWORD", "password")
 DB_HOST = environ.get("DB_HOST", "localhost")
 
-
 TESTING = environ.get("TESTING")
 
 if TESTING:
